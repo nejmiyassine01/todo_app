1f0ab6d00b071944a82fb739c1b7178d7e3ced68
nejmiyassine01 <nejmi.yassine01@gmail.com>
2020-12-09 20:17:31 +0100

    Make Clear Completed and added Element

--- src/listcontainer/ListContainer.jsx
+++ src/listcontainer/ListContainer.jsx
@@ -1,33 +1,77 @@
-import React from 'react'
+import React, { useState } from 'react'
 import * as Ai from 'react-icons/ai'
 import './ListContainer.scss'
 
-function ListContainer() {
+function ListContainer(props) {
+    const [toggleClass, setToggleClass] = useState(false);
+    const addClass = () => setToggleClass(!toggleClass);
+
+    const [remover, setRemover] = useState(false);
+    const removeItem = () => setRemover(!remover);
+
+    const [todos, setTodos] = useState([]);
+
+    const addTodo = todo => {
+        if(!todo.text || /^\s*$/.test(todo.text)) {
+            return;
+        }
+
+        const newTodos = [todo, ...todos];
+        
+        setTodos(newTodos);
+        console.log(todo, ...todos);
+    }
+    
+    const [input, setInput] = useState('');
+    const inputFields = (e) => {
+        setInput(e.target.value);
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log('Submitted Form');
+        
+        // onsubmit({
+        //     id: Math.floor(Math.random() * 10000),
+        //     text: input
+        // })
+    }
+    
     return (
         <>
-            <div className="listcontainer">
-                <form className="listcontainer_form" autocomplete="off">
+            <div className="listcontainer" onSubmit={addTodo}>
+                <form className="listcontainer_form" autoComplete="off" onSubmit={handleSubmit}>
                     <input type="checkbox" />
-                    <input className="listcontainer_input" type="text" placeholder="Enter new Item..." name="name" />
+                    <input 
+                        className="listcontainer_input" 
+                        type="text" placeholder="Enter new Item..." 
+                        onChange={inputFields} 
+                        name="name"
+                        value={input}
+                    />
+                    <button onClick={handleSubmit}>Submit</button>
                 </form>
             </div>
             
             <div className="listcontainer_todo">
-                <div className="listcontainer_items">
-                    <div className="listcontainer_content">
-                        <input type="checkbox" />
-                        <p>Clone this app</p>
+                {todos.map((todo, index) => (
+                    <div className={`listcontainer_items ${remover ? "remove" : null}`} key={index}>
+                        <div className="listcontainer_content" key={todo.id}>
+                            <input type="checkbox" />
+                            <p>{todo.text}</p>
+                        </div>
+                        <Ai.AiOutlineClose onClick={removeItem}/>
                     </div>
-                    <Ai.AiOutlineClose/>
-                </div>
+                ))}
+
                 <div className="listcontainer_list">
                     <p>0 items left</p>
                     <ul>
-                        <li><a className="active" href="#all">All</a></li>
-                        <li><a href="#active">Active</a></li>
-                        <li><a href="#completed">Completed</a></li>
+                        <li><a onClick={addClass} className={ toggleClass ? "active" : null } href="#all">All</a></li>
+                        <li><a onClick={addClass} className={ toggleClass ? "active" : null } href="#active">Active</a></li>
+                        <li><a onClick={addClass} className={ toggleClass ? "active" : null } href="#completed">Completed</a></li>
                     </ul>
-                    <a href="#clear">Clear Completed</a>
+                    <a href="#clear" className={ toggleClass ? "active" : null } onClick={removeItem}>Clear Completed</a>
                 </div>
             </div>
         </>
